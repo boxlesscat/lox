572f2edd60d14e9216a303bd41878d1106490c8d
Bh <boxlesscat@gmail.com>
2024-01-03 13:41:37 +0530

    add two ways to run lox code

---
+++ include/lox.hpp
@@ -0,0 +1,18 @@
+#ifndef LOX_HPP
+#define LOX_HPP
+
+#include <vector>
+#include <string>
+
+
+namespace lox {
+
+void run_file(const std::string&);
+
+void run_prompt();
+
+void run(const std::string&);
+
+};
+
+#endif
