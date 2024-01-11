bb21a09a750a00548671067acf02f54475bc6a8f
Bh <boxlesscat@gmail.com>
2024-01-11 15:46:17 +0530

    add runtime error function and class

--- include/error.hpp
+++ include/error.hpp
@@ -8,10 +8,18 @@
 namespace lox {
 
 static bool hadError = false;
+static bool hadRuntimeError = false;
 
 void error(const std::string, const int);
 
 void error(const Token, std::string&&);
+
+struct RuntimeError : public std::runtime_error {
+    const Token token;
+    RuntimeError(const Token token, const std::string&& message) : token(token), std::runtime_error(message) {}
+};
+
+void runtime_error(const RuntimeError);
 
 };
 
