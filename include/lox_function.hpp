9e83ee57ace2d99308647e52a0a13ec33032436b
Bh <boxlesscat@gmail.com>
2024-01-23 17:28:04 +0530

    there's closures now!

--- include/lox_function.hpp
+++ include/lox_function.hpp
@@ -9,9 +9,11 @@ namespace lox {
 class LoxFunction : public LoxCallable {
 
     const std::shared_ptr<FnStmt> declaration;
+    const std::shared_ptr<Environment> closure;
 
 public:
-    LoxFunction(const std::shared_ptr<FnStmt> declaration) : declaration(declaration) {}
+    LoxFunction(const std::shared_ptr<FnStmt> declaration, const std::shared_ptr<Environment> closure) :
+        declaration(declaration), closure(closure) {}
 
     size_t arity() override;
     std::any call(Interpreter&, std::shared_ptr<std::vector<std::any>>) override;
